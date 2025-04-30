8330edaf525f8cb49f2e7d22f2d031a9b69557d4
allania7med11 <allania7med11@gmail.com>
2025-04-30 15:04:11 +0100

    8698wv4kf__add 3 points

--- frontend/src/components/SimpleMap.tsx
+++ frontend/src/components/SimpleMap.tsx
@@ -1,27 +1,48 @@
-// src/components/SimpleMap.tsx
-'use client';
+"use client";
 
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
-import 'leaflet/dist/leaflet.css';
-import L from 'leaflet';
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
+import L from "leaflet";
 
 const markerIcon = new L.Icon({
-  iconUrl: 'https://unpkg.com/leaflet@1.9.3/dist/images/marker-icon.png',
+  iconUrl: "https://unpkg.com/leaflet@1.9.3/dist/images/marker-icon.png",
   iconSize: [25, 41],
   iconAnchor: [12, 41],
 });
 
 export default function SimpleMap() {
-  const position: [number, number] = [41.8781, -87.6298]; // Chicago
+  const coords: {
+    current: [number, number];
+    pickup: [number, number];
+    dropoff: [number, number];
+  } = {
+    current: [41.87897, -87.66063], // Chicago
+    pickup: [39.785871, -86.143448], // Indianapolis
+    dropoff: [38.633024, -90.244086], // St. Louis
+  };
 
   return (
-    <MapContainer center={position} zoom={10} style={{ height: '500px', width: '100%' }} scrollWheelZoom={true}>
+    <MapContainer
+      center={coords.current}
+      zoom={6}
+      scrollWheelZoom={true}
+      style={{ height: "500px", width: "100%" }}
+    >
       <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors'
-        url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
-      <Marker position={position} icon={markerIcon}>
-        <Popup>Chicago, IL</Popup>
+
+      <Marker position={coords.current} icon={markerIcon}>
+        <Popup>Current Location: Chicago, IL</Popup>
+      </Marker>
+
+      <Marker position={coords.pickup} icon={markerIcon}>
+        <Popup>Pickup Location: Indianapolis, IN</Popup>
+      </Marker>
+
+      <Marker position={coords.dropoff} icon={markerIcon}>
+        <Popup>Dropoff Location: St. Louis, MO</Popup>
       </Marker>
     </MapContainer>
   );
